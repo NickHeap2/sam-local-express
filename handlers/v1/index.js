4fc42f4aa3a4b5b6711901a38ea181316de4b030
Sucklead <sucklead@gmail.com>
2022-03-13 10:33:55 +0000

    feat: more tweaks for stage name, proxy has to be attached last

--- handlers/v1/index.js
+++ handlers/v1/index.js
@@ -1,29 +1,52 @@
+const util = require('util')
+
 module.exports = {
   testGet,
   testPost,
   scheduledPost,
-  pathParamTestGet
+  pathParamTestGet,
+  proxyTestGet
 }
 
 async function testGet (event) {
+  console.log('EVENT')
+  console.log(util.inspect(event))
+
   await new Promise(resolve => setTimeout(resolve, 1000))
   return getResponse('testGet')
 }
 
 async function testPost (event) {
-  return getResponse('testPost')
+  console.log('EVENT')
+  console.log(util.inspect(event))
+
+  const response = getResponse('testPost')
+  response.body = JSON.stringify(event.body)
+  return response
 }
 
 async function scheduledPost (event) {
-  return getResponse('scheduledPost')
+  const response = getResponse('scheduledPost')
+  response.body = event.body
+  return response
 }
 
 async function pathParamTestGet (event) {
+  console.log('EVENT')
+  console.log(util.inspect(event))
   const pathParameters = event.pathParameters
 
   const response = getResponse('pathParamTestGet')
   response.body = response.body.replace('OK', pathParameters.pathParam)
 
+  return response
+}
+
+async function proxyTestGet (event) {
+  console.log('EVENT')
+  console.log(util.inspect(event))
+
+  const response = getResponse('proxyTestGet')
   return response
 }
 
